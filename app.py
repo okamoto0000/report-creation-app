8eb893bfa136b01bb840a48a77bb8ccf5a6d7e30
okamoto <130630090+okamoto0000@users.noreply.github.com>
2023-04-20 11:46:09 +0900

    Fix:ok-flask-app

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def create_report():
     return render_template("index.html",message="ご利用ありがとうございます、レポートの作成が完了したことをお知らせいたします。")
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-    # app.debug = True
-    # app.run(host='localhost')
+    # port = int(os.getenv("PORT", 5000))
+    # app.run(host="0.0.0.0", port=port)
+    app.debug = True
+    app.run(host='localhost')
